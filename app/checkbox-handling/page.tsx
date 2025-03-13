a195087c8e69288e760e9d9d1b089a1a39224f3b
Raman Temre <ramantemre2110@gmail.com>
2025-03-13 13:30:13 +0530

    fix-ui: text highlight & align list

--- app/checkbox-handling/page.tsx
+++ app/checkbox-handling/page.tsx
@@ -10,7 +10,9 @@ const IncorrectCheckboxHandling = () => {
 
   return (
     <div>
-      <h1>Incorrect way of Checkbox Handling </h1>
+      <h1 className="my-2 text-lg font-semibold text-gray-900 dark:text-white">
+        Incorrect way of Checkbox Handling{" "}
+      </h1>
       <div>
         <h3>Problem Statement</h3>
         <p>
@@ -41,16 +43,25 @@ const CorrectCheckboxHandling = () => {
 
   return (
     <div>
-      <h1>Correct way of Checkbox Handling </h1>
+      <h1 className="my-2 text-lg font-semibold text-gray-900 dark:text-white">
+        Correct way of Checkbox Handling{" "}
+      </h1>
       <div>
         <h3>Solution</h3>
-        <ul>
+        <h2>
           When i click on label, the checkbox is getting checked/unchecked
+        </h2>
+        <ul className="max-w-md space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400">
           <li>
-            By binding the label to the checkbox using the &quot;htmlFor&quot;
-            attribute
+            By binding the label to the checkbox using the (htmlFor) attribute
           </li>
           <li>we can toggle the checkbox when the label is clicked.</li>
+        </ul>
+
+        <p> </p>
+        <ul>
+          <li></li>
+          <li></li>
         </ul>
       </div>
       <input
